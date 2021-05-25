99b1c0700559d68b2a149328fb7e5ce1b5d08447
Taylor Pedretti <taylor@pedretti.dev>
2021-05-25 11:10:43 -0700

    Final Code and Updated README

--- ArtOfProblemSolving_Puzzle/ArtOfProblemSolving_Puzzle.cpp
+++ ArtOfProblemSolving_Puzzle/ArtOfProblemSolving_Puzzle.cpp
@@ -2,7 +2,6 @@
 #include <fstream> 
 #include <string>
 #include <sstream> 
-#include <vector>
 #include <list>
 #include <stack>
 
@@ -11,11 +10,8 @@ using namespace std;
 struct TreeNode
 {
     int _data;
-	int _level;
     TreeNode* left;
     TreeNode* right;
-	TreeNode* previous;
-
 
 	TreeNode() {}
 	TreeNode(int data)
@@ -23,191 +19,180 @@ struct TreeNode
 		_data = data;
 		left = nullptr;
 		right = nullptr;
-		previous = nullptr;
-	}
-	TreeNode(int data, int level)
-	{
-		_data = data;
-		_level = level;
-		left = nullptr;
-		right = nullptr;
-		previous = nullptr;
 	}
 };
 
 //Read in input file info
 TreeNode* readInFile(int& target);
-void writeToFile(stack<char> path);
 
-//Process Tree and find all paths that could be taken
-void findPathes(TreeNode* root, int target, int curTotal);
+//Write outcome to file
+void writeToFile(stack<char> &path);
 
-void printPreorder(TreeNode* node, int& total);
-
-bool hasPathDiv(TreeNode* node, int target, vector<char>& path);
+//Process Tree to find if any paths lead to our given target
+bool hasPathDiv(TreeNode* node, int target, stack<char> &path);
 
 int main()
 {
-	int target;
-	int total = 1;
-	vector<char> path;
+	int target = 0;
+	stack<char> path;
 	TreeNode *root = readInFile(target);
+
 	hasPathDiv(root, target, path);
+	writeToFile(path);
+
+	return 0;
 }
 
 TreeNode* readInFile(int& target)
 {
     ifstream inFile("pyramid_sample_input.txt");
-	string curLine;
-	getline(inFile, curLine); //Get the first line for our target number
 
-	target = stoi(curLine.substr(curLine.find(" ")));
-
-	//Get root node from file
-	getline(inFile, curLine);
-	TreeNode *root = new TreeNode(stoi(curLine), 1);	
-
-	//Get all other values and assign to tree
-	list<TreeNode*> previousNodes;
-	int count = 2;
-
-	while (getline(inFile, curLine))
+	if (inFile.is_open())
 	{
-		stringstream ss(curLine);
-		list<TreeNode*> curInput;
-		list<TreeNode*> tmpHold;
+		string curLine;
+		getline(inFile, curLine); //Get the first line for our target number
 
-		for (int i; ss >> i;)
-		{
-			TreeNode* tmp = new TreeNode(i, count);
-			curInput.push_back(tmp);
+		target = stoi(curLine.substr(curLine.find(" ")));
 
-			if (ss.peek() == ',')
-				ss.ignore();
-		}
+		//Get root node from file
+		getline(inFile, curLine);
+		TreeNode* root = new TreeNode(stoi(curLine));
 
-		if (previousNodes.size() == 0) //On second line after the root
-		{
-			root->left = new TreeNode(*curInput.front());
-			root->left->_level = count;
-			root->right = new TreeNode(*curInput.back());
-			root->right->_level = count;
+		//Hold onto the previous nodes the line before
+		list<TreeNode*> previousNodes;
 
-			previousNodes.push_back(root->left);
-			previousNodes.push_back(root->right);
-		}
-		else
-		{
-			for (TreeNode* pN : previousNodes)
+		while (getline(inFile, curLine))
+		{			
+			//stringstream used to make it easier to parse input file
+			stringstream ss(curLine); 
+			//curLine input stored in a list of TreeNodes
+			list<TreeNode*> curInput; 
+			//Used to temporarily curInput to then replace all in previousNodes
+			list<TreeNode*> tmpHold; 
+
+			//Process the curLine from line and split it at each comma
+			for (int i; ss >> i;)
 			{
-				pN->left = curInput.front();
-				tmpHold.push_back(curInput.front());
-				curInput.pop_front();
-				pN->right = curInput.front();
+				TreeNode* tmp = new TreeNode(i);
+				curInput.push_back(tmp);
+
+				if (ss.peek() == ',')
+					ss.ignore();
 			}
 
-			previousNodes.clear();
-			tmpHold.push_back(curInput.front());
-			previousNodes = tmpHold;
-			curInput.clear();
-			tmpHold.clear();
+			//Process the second line from the line
+			if (previousNodes.size() == 0) 
+			{
+				root->left = new TreeNode(*curInput.front());
+				root->right = new TreeNode(*curInput.back());
+
+				previousNodes.push_back(root->left);
+				previousNodes.push_back(root->right);
+			}
+			//Process everything from 3rd line and lower
+			else 
+			{
+				//Assign all curNodes to the previous nodes before it
+				for (TreeNode* pN : previousNodes) 
+				{
+					pN->left = curInput.front();
+					tmpHold.push_back(curInput.front());
+					curInput.pop_front();
+					pN->right = curInput.front();
+				}
+
+				//Clear all last previousNode list
+				previousNodes.clear(); 
+				//Push the last curInput node onto the tmp holding
+				tmpHold.push_back(curInput.front()); 
+				//All nodes in tmpHold which are curInput to previousNodes
+				previousNodes = tmpHold; 
+				//Clear curInput
+				curInput.clear(); 
+				//Clear tmp holding
+				tmpHold.clear(); 
+			}
 		}
 
-		count++;
+		inFile.close();
+		return root;
+	}
+	else
+	{
+		cout << "Failed to open in file!" << endl;
 	}
 
-	return root;
+	inFile.close();
+	return nullptr;
 }
 
-void findPathes(TreeNode* root, int target, int curTotal)
+void writeToFile(stack<char> &path)
 {
-	if (root == NULL)
+	ofstream outFile("pyramid_sample_output.txt");
+
+	if (outFile.is_open())
 	{
-		return;
-	}
-
-	stack<TreeNode*> nodeStack;
-	nodeStack.push(root);
-	curTotal = root->_data;
-
-	while (!nodeStack.empty())
-	{
-		TreeNode* node = nodeStack.top();
-		cout << "{L: " << node->_level << ", D: " << node->_data << "}" << endl;
-
-		nodeStack.pop();
-
-		if (node->right)
-			nodeStack.push(node->right);
-		else
+		while (!path.empty())
 		{
-
-		}
-
-
-		if (node->left)
-			nodeStack.push(node->left);
-		else
-		{
-
+			outFile << path.top();
+			path.pop();
 		}
 	}
-}
-
-void printPreorder(TreeNode* node, int &total)
-{
-	if (node->left == NULL && node->right == NULL)
+	else
 	{
-		cout << node->_data << " ";
-		return;
+		cout << "Failed to write to file!" << endl;
 	}
 
-	/* first print data of node */
-	cout << node->_data << " ";
-
-	/* then recur on left sutree */
-	printPreorder(node->left, total);
-
-	/* now recur on right subtree */
-	printPreorder(node->right, total);
+	outFile.close();
 }
 
-bool hasPathDiv(TreeNode* node, int target, vector<char>& path)
+bool hasPathDiv(TreeNode* node, int target, stack<char> &path)
 {
-	if (node == nullptr)
+	//Check to main see if root is null
+	if (node == nullptr) 
 	{
 		return (target == 0);
 	}
 	else
 	{
 		bool ans = false;
-		float subSum = target / node->_data;		
+		//Keep track of the recursion sum while processing
+		float subSum = target / node->_data; 
 
-		if (subSum == 0 && node->left == nullptr && node->right == nullptr)
+		//If our subSum is 0 and doesn't have nodes on the left or right then valid path
+		if (subSum == 0 && node->left == nullptr && node->right == nullptr) 
 			return true;
 
-		if (node->left)
+		//Go down left side of node
+		if (node->left) 
 		{
-			ans = ans;
-			path.push_back('L');			
-			hasPathDiv(node->left, subSum, path);
-			path.pop_back();
+			//If we haven't found a the end of the path and if it isn't valid 
+			if (ans == false) 
+			{
+				path.push('L');
+				ans = hasPathDiv(node->left, subSum, path);
+			}
+
+			//If we have a valid path dont pop the path off the stack
+			if(ans == false) 
+				path.pop();
 		}
 
-		if (node->right)
+		//Go down right side of node
+		if (node->right) 
 		{
-			ans = ans;
-			path.push_back('R');			
-			hasPathDiv(node->right, subSum, path);
-			path.pop_back();
-		}
+			//If we haven't found a the end of the path and if it isn't valid
+			if (ans == false)  
+			{
+				path.push('R');
+				ans = hasPathDiv(node->right, subSum, path);
+			}
 
+			//If we have a valid path dont pop the path off the stack
+			if (ans == false) 
+				path.pop();
+		}
 		
 		return ans;
 	}
-}
-
-void writeToFile(stack<char> path)
-{
-
 }
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ArtOfProblemSolving_Puzzle
 https://artofproblemsolving.com/careers/job/4253969003_software_engineer
 
+When applying was asking to solve puzzle before submitting application. Solved using DFS with a check on if our subtotal goes to zero and that we are at the end of the path for a branch. 
 
 Used these to help solve:
 https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
